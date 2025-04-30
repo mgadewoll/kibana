0c4bd2bc5c09b34d4856265184a56a4e172cd4bc
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-04-30 17:23:17 +0200

    refactor: update custom button styles to align with Visual Refresh button updates

--- src/platform/packages/shared/kbn-unified-field-list/src/components/field_list_filters/field_type_filter.tsx
+++ src/platform/packages/shared/kbn-unified-field-list/src/components/field_list_filters/field_type_filter.tsx
@@ -28,6 +28,9 @@ import {
   EuiButtonEmpty,
   useEuiTheme,
   EuiTitle,
+  logicalCSS,
+  UseEuiTheme,
+  mathWithUnits,
 } from '@elastic/eui';
 import type { CoreStart } from '@kbn/core-lifecycle-browser';
 import { FormattedMessage } from '@kbn/i18n-react';
@@ -48,7 +51,21 @@ const EQUAL_HEIGHT_OFFSET = 2; // to avoid changes in the header's height after 
 const popoverTitleStyle = css`
   padding: ${EQUAL_HEIGHT_OFFSET}px 0;
 `;
-const filterButtonStyle = css`
+
+const filterPopoverStyle = ({ euiTheme }: UseEuiTheme) => css`
+  .euiFilterButton__wrapper {
+    ${logicalCSS('left', `-${euiTheme.size.s}`)}
+    ${logicalCSS('width', `calc(100% + ${mathWithUnits(euiTheme.size.s, (x) => x * 2)})`)}
+
+    &::before {
+      display: none;
+    }
+  }
+`;
+
+const filterButtonStyle = ({ euiTheme }: UseEuiTheme) => css`
+  ${logicalCSS('width', '100%')}
+
   &,
   & .euiFilterButton__text {
     min-width: 0;
@@ -158,12 +175,13 @@ export function FieldTypeFilter<T extends FieldListItem = DataViewField>({
       display="block"
       isOpen={isOpen}
       closePopover={() => setIsOpen(false)}
+      css={filterPopoverStyle}
       button={
         <EuiFilterButton
           aria-label={i18n.translate('unifiedFieldList.fieldTypeFilter.filterByTypeAriaLabel', {
             defaultMessage: 'Filter by type',
           })}
-          color="primary"
+          color="text"
           isSelected={isOpen}
           numFilters={selectedFieldTypes.length}
           hasActiveFilters={!!selectedFieldTypes.length}
