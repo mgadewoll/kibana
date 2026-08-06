e1e81f23116a53b686ef5f3c83d33e59853fffde
Lene Gadewoll <lene.gadewoll@elastic.co>
2026-08-06 15:27:59 +0200

    feat: add type-to-callout map for easier usage/migration

--- src/platform/kbn-ui/callout/index.ts
+++ src/platform/kbn-ui/callout/index.ts
@@ -7,7 +7,29 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { KbnInfoCallout, type KbnInfoCalloutProps } from './src/info_callout';
-export { KbnSuccessCallout, type KbnSuccessCalloutProps } from './src/success_callout';
-export { KbnWarningCallout, type KbnWarningCalloutProps } from './src/warning_callout';
-export { KbnDangerCallout, type KbnDangerCalloutProps } from './src/danger_callout';
+import { KbnInfoCallout, type KbnInfoCalloutProps } from './src/info_callout';
+import { KbnSuccessCallout, type KbnSuccessCalloutProps } from './src/success_callout';
+import { KbnWarningCallout, type KbnWarningCalloutProps } from './src/warning_callout';
+import { KbnDangerCallout, type KbnDangerCalloutProps } from './src/danger_callout';
+
+export {
+  KbnInfoCallout,
+  KbnSuccessCallout,
+  KbnWarningCallout,
+  KbnDangerCallout,
+  type KbnInfoCalloutProps,
+  type KbnSuccessCalloutProps,
+  type KbnWarningCalloutProps,
+  type KbnDangerCalloutProps,
+};
+
+/**
+ * Utility map for type-to-callout mapping.
+ * This helps prevent implementations from having to repeat the mapping.
+ */
+export const kbnCalloutMap = {
+  primary: KbnInfoCallout,
+  success: KbnSuccessCallout,
+  warning: KbnWarningCallout,
+  danger: KbnDangerCallout,
+} as const;
